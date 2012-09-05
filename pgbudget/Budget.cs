9a11cf7f5a0575161c10006b6b391c88850ad150
buttonwang <buttonwang@gmail.com>
2012-09-05 16:37:05 +0800

    修改工程打开功能

---
+++ pgbudget/Budget.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//工程分为建筑、安装、拆除工程 (jz, az, cc)
+
+namespace pgbudget
+{
+    class Budget
+    {
+        readonly PGDataClassesDataContext db = new PGDataClassesDataContext();
+        
+        private TreeNode findNode;
+
+        private TreeNode FindRecursive(TreeNode treeNode, int pid)
+        {
+            if ((int)treeNode.Tag == pid) findNode = treeNode;
+
+            foreach (TreeNode tn in treeNode.Nodes)
+            {
+                if ((int)tn.Tag == pid) findNode = tn; 
+                else FindRecursive(tn, pid);
+            }
+
+            return findNode;
+        }
+
+        public TreeNode IndexTree(String indexName)
+        {
+
+            if (indexName == "jz")
+            {
+                var node = new TreeNode { Text = "项目拆分表", Tag = 0 };
+                                
+                var query = from jzgc in db.jzgcs
+                        select new {mc=jzgc.mc, ID=jzgc.id, ParentID = jzgc.pid};
+
+                foreach (var jzindex in query)
+                {
+                    findNode = null;
+                    FindRecursive(node, jzindex.ParentID.Value);
+                    if (findNode != null) findNode.Nodes.Add(new TreeNode { Text = jzindex.mc, Tag = jzindex.ID });
+                    else node = new TreeNode { Text = jzindex.mc, Tag = jzindex.ID };
+                }
+
+                return node;
+            }
+            
+            return null;
+        }
+    }
+
+}
---
+++ pgbudget/Const.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pgbudget
+{
+    class Const
+    {
+        public static MainForm mainForm;
+
+        //工程项目列标题
+        public static readonly string[] ysColumns = { "类型", "代码", "标记", "名称", "单位", "计算式", "数量",
+                                                      "定额基价", "装材预算价", "设备原价", "定额合价", "装材合价", "设备合价",
+                                                      "总工日数", "线路类型", "备注"};
+
+
+
+
+    }
+   
+}
